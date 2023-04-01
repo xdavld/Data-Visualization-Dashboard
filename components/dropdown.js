406e2dbe8b59cbc77b96237f7644f4eb6654f356
xdavId <davidkreismann@gmx.de>
2023-04-01 17:02:20 +0200

    first commit

--- components/dropdown.js
+++ components/dropdown.js
@@ -1,12 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const selectElement = document.getElementById("form-field2");
-  selectElement.addEventListener("change", () => {
-    const selectedOption = selectElement.value;
-    if (selectedOption === "Flugdaten") {
-      window.location.href = "./index.html";
-    } else if (selectedOption === "Wetterdaten") {
-      window.location.href = "./pages/flug_wetterdaten.html";
-    }
-  });
-});
+/*Toggle dropdown list*/
+/*https://gist.github.com/slavapas/593e8e50cf4cc16ac972afcbad4f70c8*/
 
+var userMenuDiv = document.getElementById("userMenu");
+var userMenu = document.getElementById("userButton");
+
+document.onclick = check;
+
+function check(e) {
+  var target = (e && e.target) || (event && event.srcElement);
+
+  //User Menu
+  if (!checkParent(target, userMenuDiv)) {
+    // click NOT on the menu
+    if (checkParent(target, userMenu)) {
+      // click on the link
+      if (userMenuDiv.classList.contains("invisible")) {
+        userMenuDiv.classList.remove("invisible");
+      } else {
+        userMenuDiv.classList.add("invisible");
+      }
+    } else {
+      // click both outside link and outside menu, hide menu
+      userMenuDiv.classList.add("invisible");
+    }
+  }
+}
+
+function checkParent(t, elm) {
+  while (t.parentNode) {
+    if (t == elm) {
+      return true;
+    }
+    t = t.parentNode;
+  }
+  return false;
+}
