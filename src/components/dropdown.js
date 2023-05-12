1388f58e35fff3bd292f7f5c1425df84fa3e5829
xdavId <davidkreismann@gmx.de>
2023-05-12 04:48:28 +0200

    push

---
+++ src/components/dropdown.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const selectElement = document.getElementById("form-field2");
+  selectElement.addEventListener("change", () => {
+    const selectedOption = selectElement.value;
+    if (selectedOption === "Flugdaten") {
+      window.location.href = "./index.html";
+    } else if (selectedOption === "Wetterdaten") {
+      window.location.href = "./pages/flug_wetterdaten.html";
+    }
+  });
+});
+
